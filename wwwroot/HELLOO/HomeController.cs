fe07eb77094f3405abefa00cc4e5c8c4a546f9c0
ithikiloveaz123 <thuanphanhoangminh4566@gmail.com>
2024-04-10 14:17:47 +0700

    bổ sung thêm controller

---
+++ wwwroot/HELLOO/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_DA.wwwroot.HELLOO
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
